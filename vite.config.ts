83c99416f5cc1cfe527ff9ddde68ecde66c2b59d
brundagejoe <joebrundage@icloud.com>
2024-06-24 16:49:22 -0600

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/run-tracker/",
   plugins: [
     remix({
+      basename: "/run-tracker/",
       ssr: false,
       future: {
         v3_fetcherPersist: true,
